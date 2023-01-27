307e6cab7f17256648a3b0b979bec3754ce89c70
prakuldip <114090881+prakuldip@users.noreply.github.com>
2023-01-27 08:58:50 +0530

    docker command check script is added
    check docker commands if executable

---
+++ practice/checkdocker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "we arechecking if docker command can be executed in this container"
+docker container ls
+if [ $? == 0 ]; then
+echo "docker command can be executed in this container"
+else
+echo "docker commands can not be executed in this container"
